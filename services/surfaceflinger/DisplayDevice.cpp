779a26eb8bcb49f6f8f9fe956f72a7dab825a0a3
Andrew Dodd <atd7@cornell.edu>
2012-12-10 08:26:54 -0500

    DisplayDevice: Backwards compatibility with old EGL
    From 4.1 to 4.2, the display subsystem was reworked to
    use SurfaceTextureClient/BufferQueue instead of
    FramebufferNativeWindow for the framebuffer itself.
    
    Unfortunately, some legacy EGL libraries make assumptions
    that any framebuffer device will be FramebufferNativeWindow.
    
    These EGL libraries will fail when used in 4.2 as if the
    framebuffer is not FramebufferNativeWindow, they will
    try to dequeue more than one buffer at a time, which
    will cause a hang of the graphics subsystem.
    
    This allows use of FramebufferNativeWindow to keep
    legacy EGL implementations happy.  Confirmed EGL
    implementations that need this include but are
    not limited to:
    
    Set BOARD_EGL_NEEDS_FNW to use
    
    Tegra2/3 ICS EGL
    Mali400 ICS EGL (Allwinner A10)
    Mali400 Jellybean (4.1) EGL (Samsung Exynos4)
    Adreno Jellybean (4.1) EGL (legacy qcom devices using PMEM)
    
    Original implementation by OndraOrg of XDA:
    http://forum.xda-developers.com/showpost.php?p=34469675&postcount=19
    
    Change-Id: If7c455035a002f272fa1a8af1cfcc60eb7ade894
    
    Conflicts:
    
    	services/surfaceflinger/Android.mk
    	services/surfaceflinger/DisplayDevice.cpp

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -29,7 +29,7 @@
 
 #include <gui/Surface.h>
 
-#ifdef BOARD_EGL_NEEDS_LEGACY_FB
+#ifdef EGL_NEEDS_FNW
 #include <ui/FramebufferNativeWindow.h>
 #endif
 
@@ -78,7 +78,11 @@ DisplayDevice::DisplayDevice(
       mOrientation()
 {
     mNativeWindow = new Surface(producer, false);
+#ifndef EGL_NEEDS_FNW
     ANativeWindow* const window = mNativeWindow.get();
+#else
+    ANativeWindow* const window = new FramebufferNativeWindow();
+#endif
 
     int format;
     window->query(window, NATIVE_WINDOW_FORMAT, &format);
