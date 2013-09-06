9436c06452cd1a218305b5b84eb0abd0c88c6faf
Tom Briden <tom@decompile.me.uk>
2013-09-06 13:53:16 +0100

    surfaceflinger: don't handle EVENT_ORIENTATION when !HWC_DEVICE_API_VERSION_1_0
    a184463 causes segfault on boot for the enrc2b (and probably others), this fixes it.
    
    Change-Id: Ic4a7a05b44c8cc44a0d25c56def4acdf5b0939df

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -623,7 +623,7 @@ void HWComposer::eventControl(int disp, int event, int enabled) {
                     const int32_t oldValue = mDisplayData[disp].events & eventBit;
                     if (newValue != oldValue) {
                         ATRACE_CALL();
-                        err = mHwc->eventControl(mHwc, disp, event, enabled);
+                        err = mHwc->eventControl ? mHwc->eventControl(mHwc, disp, event, enabled) : NO_ERROR;
                         if (!err) {
                             int32_t& events(mDisplayData[disp].events);
                             events = (events & ~eventBit) | newValue;
@@ -644,7 +644,8 @@ void HWComposer::eventControl(int disp, int event, int enabled) {
             break;
         case EVENT_ORIENTATION:
             // Orientation event
-            err = mHwc->eventControl(mHwc, disp, event, enabled);
+            if (hwcHasApiVersion(mHwc, HWC_DEVICE_API_VERSION_1_0))
+              err = mHwc->eventControl(mHwc, disp, event, enabled);
             break;
         default:
             ALOGW("eventControl got unexpected event %d (disp=%d en=%d)",
@@ -898,15 +899,16 @@ status_t HWComposer::release(int disp) {
     if (mHwc) {
         if (hwcHasVsyncEvent(mHwc)) {
             eventControl(disp, HWC_EVENT_VSYNC, 0);
+	    if (mHwc->blank)
+		return (status_t)mHwc->blank(mHwc, disp, 1);
         }
-        return (status_t)hwcBlank(mHwc, disp, 1);
     }
     return NO_ERROR;
 }
 
 status_t HWComposer::acquire(int disp) {
     LOG_FATAL_IF(disp >= VIRTUAL_DISPLAY_ID_BASE);
-    if (mHwc) {
+    if (mHwc && mHwc->blank) {
         return (status_t)hwcBlank(mHwc, disp, 0);
     }
     return NO_ERROR;
